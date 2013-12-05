1bc51b014f6ca9209c3a625936425a2f09a7da34
Danesh M <daneshm90@gmail.com>
2013-12-04 18:02:35 -0800

    Bringup for 4.4
    Change-Id: I5d46664f5fc51743d7c2e146f40a0fa0a20671c8

--- factory-images/generate-factory-images-package.sh
+++ factory-images/generate-factory-images-package.sh
@@ -1,5 +1,6 @@
-#
-# Copyright (C) 2011 The Android Open-Source Project
+#!/bin/sh
+
+# Copyright 2013 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,19 +13,24 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
-TARGET_BOOTLOADER_BOARD_NAME := deb
-TARGET_BOOTLOADER_NAME := deb
-TARGET_BOARD_INFO_FILE := device/asus/deb/board-info.txt
+# start jb-mr2-dev
+# 704765 = JSR72
+# 740015 = JSS11F
+# 741000 = JSS11I
+# 741250 = JSS15
+# 746990 = JSS15H
+# 748502 = JSS15I
+# 748593 = JSS15J
+# 750418 = JSS15K
+# end jb-mr2-dev
 
-BOARD_HAL_STATIC_LIBRARIES := libdumpstate.deb
-
-# TARGET_RECOVERY_UI_LIB := librecovery_ui_deb
-
-TARGET_RELEASETOOLS_EXTENSIONS := device/asus/deb
-
-TARGET_RECOVERY_FSTAB = device/asus/deb/recovery.fstab
-
--include vendor/asus/deb/BoardConfigVendor.mk
-include device/asus/flo/BoardConfigCommon.mk
+source ../../../common/clear-factory-images-variables.sh
+BUILD=750418
+DEVICE=deb
+PRODUCT=razorg
+VERSION=jss15k
+SRCPREFIX=signed-
+BOOTLOADER=flo-03.14
+RADIO=deb-g00_1.23.0_0720
+source ../../../common/generate-factory-images-common.sh
