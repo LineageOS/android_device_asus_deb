e29dd4e35f39804f8bbbe421a32c5f6c29a197b1
Georg Semmler <weiznich@gmail.com>
2014-12-03 10:55:42 +0100

    Merge aosp_5.0.1_r1
    Change-Id: I2bf93203323335839ccb984b0415c9e85a59e414

--- aosp_deb.mk
+++ aosp_deb.mk
@@ -17,6 +17,8 @@
 # Sample: This is where we'd set a backup provider if we had one
 # $(call inherit-product, device/sample/products/backup_overlay.mk)
 
+# Inherit from hardware-specific part of the product configuration
+$(call inherit-product, device/asus/deb/device.mk)
 
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
@@ -26,9 +28,8 @@ PRODUCT_DEVICE := deb
 PRODUCT_BRAND := Android
 PRODUCT_MODEL := Nexus 7
 PRODUCT_MANUFACTURER := ASUS
-PRODUCT_RESTRICT_VENDOR_FILES := false
+PRODUCT_RESTRICT_VENDOR_FILES := false 
 
 # Inherit from hardware-specific part of the product configuration
-$(call inherit-product, device/asus/deb/device.mk)
 $(call inherit-product-if-exists, vendor/asus/deb/device-vendor.mk)
 #$(call inherit-product-if-exists, vendor/qcom/proprietary/common/config/device-vendor.mk)
