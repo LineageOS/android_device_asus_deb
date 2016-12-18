3ef5d9f8fdb541d8a434f90c3c8a19125b9f74db
Simon Shields <keepcalm444@gmail.com>
2016-12-18 16:09:12 +1100

    deb: commonize overlays with flo
    Change-Id: I023b8cd9dee8bb8a7cb28fbee3776a50ad3ce039

--- device.mk
+++ device.mk
@@ -42,4 +42,4 @@ $(call inherit-product, device/asus/flo/device-common.mk)
 # inherit from the non-open-source side, if present
 $(call inherit-product-if-exists, vendor/asus/deb/deb-vendor.mk)
 
-DEVICE_PACKAGE_OVERLAYS := device/asus/deb/overlay
+DEVICE_PACKAGE_OVERLAYS += device/asus/deb/overlay
