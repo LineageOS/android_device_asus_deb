994a4d58f216f5e4576a49e11a4f41f242a487f5
Jaekyun Seok <jaekyun@google.com>
2017-04-05 11:23:26 +0900

    deb: Enforce RRO on framework-res
    Overlays only for framework-res will be converted into RROs.
    
    Other overlays can't be converted due to some known issues on app RRO.
    
    Bug: 36231603
    Change-Id: Idca30fdbbcc990fd124de16a06a112a346612a61
    (cherry picked from commit db1b6058200fc5255f519cf6609fca7619d6743e)

--- device.mk
+++ device.mk
@@ -43,3 +43,5 @@ $(call inherit-product, device/asus/flo/device-common.mk)
 $(call inherit-product-if-exists, vendor/asus/deb/deb-vendor.mk)
 
 DEVICE_PACKAGE_OVERLAYS := device/asus/deb/overlay
+PRODUCT_ENFORCE_RRO_TARGETS := \
+    framework-res
