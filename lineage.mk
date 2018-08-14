e490f3152a6d31f604e97c8f5151bdbb07a68126
ripee <ripee.dev@gmail.com>
2018-08-15 00:12:41 +0200

    Deb: Fix vendor image mismatch
    Needed after https://review.lineageos.org/#/c/208868/
    
    Change-Id: Iae14bc781e6171fccac92c7190abc9b6de06c4b0

--- lineage.mk
+++ lineage.mk
@@ -26,5 +26,6 @@ PRODUCT_MANUFACTURER := asus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=razorg \
-    BUILD_FINGERPRINT=google/razorg/deb:6.0.1/MOB30X/3036618:user/release-keys \
     PRIVATE_BUILD_DESC="razorg-user 6.0.1 MOB30X 3036618 release-keys"
+
+BUILD_FINGERPRINT := google/razorg/deb:6.0.1/MOB30X/3036618:user/release-keys
